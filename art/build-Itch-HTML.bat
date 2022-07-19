4ed13ea471c04781c5bc870c76af555499f0d986
Newpersonhere <97367726+Newpersonhere@users.noreply.github.com>
2022-07-19 08:20:18 -0400

    Update build-Itch-HTML.bat

--- art/build-Itch-HTML.bat
+++ art/build-Itch-HTML.bat
@@ -7,5 +7,5 @@ lime build html5 -debug
 echo UPLOADING TO ITCH
 butler push ./export/debug/html5/bin ninja-muffin24/funkin:html5
 butler status ninja-muffin24/funkin:html5
-echo ITCH SHIT UPDATED LMAOOOOO
+echo ITCH SH*T UPDATED LMAOOOOO
 pause
